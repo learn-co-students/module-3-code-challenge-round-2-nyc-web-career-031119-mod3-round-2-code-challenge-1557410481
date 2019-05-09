91082c251d744a26a77c783e69a0d1148c52df82
github username <github email address>
2019-05-09 10:56:08 -0400

    hit first 2 delivs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 2587 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+  const imageCard = document.getElementById("image_card")
+  const imageHolder = document.getElementById("image")
+  const imageName = document.getElementById("name")
+  let imageLikes = document.getElementById("likes")
+  const imageComments = document.getElementById("comments")
+  // const likeButton = imageCard.getElementById('like_button')
+
+  fetch(`${imageURL}`)
+  .then(resp => resp.json())
+  .then(function(image){
+    // debugger
+    renderImage(image)
+  })
+
+
+
+
+
+
+//EventListeners
+imageCard.addEventListener('click', function(e){
+
+  // let like_count = 0
+  const likeButton = e.target.id
+  if(likeButton === "like_button"){
+    // console.log(parseInt(imageLikes.innerText))
+    numLikes = parseInt(imageLikes.innerText)
+    // debugger
+    ++numLikes
+   imageLikes.innerText = numLikes
+
+  }
+
+})
+
+
+
+
+
+
+
+
+//helper function===============
+  //helper function to render image
+  function renderImage(image){
+    imageHolder.src = `${image.url}`
+    imageName.innerText = `${image.name}`
+    imageLikes.innerText = `${image.like_count}`
+    findAllComments(image.comments)
+  }
+
+function findAllComments(comments){
+  comments.forEach(function(comment){
+    imageComments.innerHTML += `<li>${comment.content}</li>`
+  })
+}
+
+
+
+
+
+
+
 
 })
